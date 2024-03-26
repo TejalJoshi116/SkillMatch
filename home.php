a4e8f7bc955c4e5e1970bcadcb5b40e0ca7e23c5
ApurvaH <apurvah1003@gmail.com>
2024-03-26 15:34:05 +0530

    Homepage commit

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Skill Match</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <div class="topframe"> 
+        <div class="top" id="logo">
+            <img src="SkillMatch_logo.png" alt="Skill Match logo" height="100px" width="100px">
+        </div> 
+        <div class="top" id="heading">
+            <h1>Skill Match</h1>
+            <a href="client_login.php" class="btn btn-light" role="button">Client Login</a>
+            <a href="professional_login.php" class="btn btn-light" role="button">Student/Professional Login</a>
+        </div>
+    </div>
+
+    <iframe class="details" src="details.php" frameborder="0" width="40%" height="600px"></iframe>
+    <iframe class="slides" src="slides.php" frameborder="0" width="59.6%" height="600px"></iframe>
+</body>
+</html>
